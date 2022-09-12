98bf586b4e76ef858c847412da45c38966205926
Vanessa Barone <vanbarone.milani@gmail.com>
2022-09-12 15:59:06 -0300

    Acero das configurações nas classas 'Startup' e 'App Settings'

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,10 +29,16 @@ namespace ConsultaVeterinariaAPI
         {
 
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ConsultaVeterinariaAPI", Version = "v1" });
             });
+
+            services.AddDbContext<ConsultaVeterinariaAPI>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"))
+            );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
