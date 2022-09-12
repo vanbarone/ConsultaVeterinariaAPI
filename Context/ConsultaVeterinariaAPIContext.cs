2f2c045c643ae6f4e5b489a08139b75ffd1a588a
Vanessa Barone <vanbarone.milani@gmail.com>
2022-09-12 15:53:21 -0300

    Criação do Context

---
+++ Context/ConsultaVeterinariaAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using ConsultaVeterinariaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsultaVeterinariaAPI.Context
+{
+    public class ConsultaVeterinariaAPIContext: DbContext
+    {
+        public ConsultaVeterinariaAPIContext(DbContextOptions<ConsultaVeterinariaAPIContext> options): base(options)
+        {
+        }
+
+        public DbSet<TipoUsuario> TipoUsuario { get; set; }
+
+        public DbSet<Especialidade> Especialidade { get; set; }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        
+        public DbSet<Medico> Medico { get; set; }
+        
+        public DbSet<Paciente> Paciente { get; set; }
+
+        public DbSet<Consulta> Consulta { get; set; }
+
+    }
+}
